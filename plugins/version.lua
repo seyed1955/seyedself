4238f05ecf2fa4b44cfb0ecab2202bba1fa7aa3a
alireza1998 <alireza.eidani1000@gmail.com>
2016-04-04 05:03:23 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-    return "Self-Bot v2 \n by BeatBot Team :) <3 \n @BeatBot_Team"
+    return "Self-Bot v2 \n by Security Team :) <3 \n @create_antispam_bot"
 end 
 return {
   patterns = {
