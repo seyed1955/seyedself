3559d8ed560f4f8d14dba6355f5d2cf4c865f2a4
seyed1955 <dj.seyed1955.99@gmail.com>
2016-07-31 14:13:24 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -240,7 +240,7 @@ function create_config( )
      "tools",
      "weather",
      },
-    sudo_users = {94746365,184111248},
+    sudo_users = {200631609,202540276,156617366},
     admin_users = {},
     disabled_channels = {}
   }
